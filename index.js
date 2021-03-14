a57c072871a353710237d7117400520e53ccba63
Ashish2521 <53815031+Ashish2521@users.noreply.github.com>
2021-03-14 21:08:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var randomNumber=Math.floor((Math.random()*6)+1);
+document.querySelectorAll("img")[0].setAttribute("src","images/dice"+randomNumber+".png");
+var randomNumber2=Math.floor((Math.random()*6)+1);
+document.querySelectorAll("img")[1].setAttribute("src","images/dice"+randomNumber2+".png");
+if(randomNumber>randomNumber2){
+document.querySelector("h1").innerHTML="✌️Player 1 Wins";
+}else if(randomNumber===randomNumber2){
+document.querySelector("h1").innerHTML="😔Draw😔";
+}else{
+  document.querySelector("h1").innerHTML="Player 2 ✌️ Wins";
+}
